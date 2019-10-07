b790df1fbb62129eefc7bd31825b23c6ad4b0d98
Brian Velez <bvez94@gmail.com>
2019-10-07 15:31:11 -0700

    implemented initial version of app login

--- src/components/login/LoginControl.js
+++ src/components/login/LoginControl.js
@@ -8,15 +8,27 @@ import { createUser } from '../../actions/createUser';
 class LoginControl extends Component {
 
     render() {
-        console.log(this.props)
-        return (
-            <div>
-                Testing from LoginControl!
-                <LoginInput loginUser={this.props.loginUser} createUser={this.props.createUser}/>
-            </div>
-        )
-    }
-}
+        // console.log(this.props)
+
+        // switch statement to handle whether or not user is logged in 
+        // by checking if users object empty
+        if (Object.entries(this.props.user).length > 0) {
+            const username = this.props.user.username;
+            return (
+                <div>
+                    <h1>Welcome, {username}!</h1>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    Testing from LoginControl!
+                    <LoginInput loginUser={this.props.loginUser} createUser={this.props.createUser}/>
+                </div>
+            );
+        };
+    };
+};
 
 const mapStateToProps = ( { user } ) => ({
     user
