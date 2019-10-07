2eac34075a6de21663ddccb3ceb676377bc65d02
Brian Velez <bvez94@gmail.com>
2019-10-07 16:01:06 -0700

    added router to app. created route for loginControl component.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import LoginControl from './components/login/LoginControl';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <p>Testing from App!</p>
-      <LoginControl />
-    </div>
+    <Router>
+      <div className="App">
+        {/* <p>Testing from App!</p> */}
+        {/* <LoginControl /> */}
+        <Route exact path ='/' component={LoginControl}/>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
