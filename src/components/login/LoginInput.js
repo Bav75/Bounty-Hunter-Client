96549bca89a84e2f7d587ae3d9b88c2cf685c500
Brian Velez <bvez94@gmail.com>
2019-10-07 15:11:18 -0700

    refactored LoginInput to conditionally render login and create user forms.

--- src/components/login/LoginInput.js
+++ src/components/login/LoginInput.js
@@ -4,7 +4,8 @@ export default class LoginInput extends Component {
 
     state = {
         username: '',
-        password: ''
+        password: '',
+        form: ''
     };
 
     handleOnChange = ({ target }) => {
@@ -16,7 +17,7 @@ export default class LoginInput extends Component {
 
     handleOnSubmit = (event) => {
         event.preventDefault();
-        console.log(event.target.name);
+        // console.log(event.target.name);
         switch (event.target.name) {
             case 'Login':
                 this.props.loginUser(this.state);
@@ -28,24 +29,59 @@ export default class LoginInput extends Component {
         // reset state after entering 
         this.setState({
             username: '',
-            password: ''
+            password: '',
+            form: ''
         });
     };
 
-
     render() {
-        return (
-            <div>
-                Testing from LoginInput!
-                <form onSubmit={this.handleOnSubmit}>
-                    Username:<input type="text" value={this.state.username} name="username" onChange={this.handleOnChange}/>
-                    <br/>
-                    Password:<input type="password" value={this.state.password} name="password" onChange={this.handleOnChange}/>
-                    <br/>
-                    <input type="submit" value="Login" name="Login"/>
-                    <input type="submit" value="Create New Account" name="Create"/>
-                </form>
-            </div>
-        )
-    }
-}
+
+        switch (this.state.form) {
+            case 'LOGIN':
+                return (
+                    <div>
+                        <form onSubmit={this.handleOnSubmit} name="Login">
+                        Username:<input type="text" value={this.state.username} name="username" onChange={this.handleOnChange}/>
+                        <br/>
+                        Password:<input type="password" value={this.state.password} name="password" onChange={this.handleOnChange}/>
+                        <br/>
+                        <input type="submit" value="Login" /></form>
+                    </div>
+                );
+            case 'CREATE':
+                return (
+                    <div>
+                        <form onSubmit={this.handleOnSubmit} name="Create">
+                        Username:<input type="text" value={this.state.username} name="username" onChange={this.handleOnChange}/>
+                        <br/>
+                        Password:<input type="password" value={this.state.password} name="password" onChange={this.handleOnChange}/>
+                        <br/>
+                        <input type="submit" value="Create Account" /></form>
+                    </div>
+                );
+            default: 
+                return (
+                    <div>
+                            <button onClick={() => {this.setState({form: 'LOGIN'})}}>Login</button>
+                            <button onClick={() => {this.setState({form: 'CREATE'})}}>Create New Account</button>
+                    </div>
+                );
+        };
+    };
+};
+
+        // return (
+        //     <div>
+        //         {/* Testing from LoginInput!
+        //         <form onSubmit={this.handleOnSubmit}>
+        //             Username:<input type="text" value={this.state.username} name="username" onChange={this.handleOnChange}/>
+        //             <br/>
+        //             Password:<input type="password" value={this.state.password} name="password" onChange={this.handleOnChange}/>
+        //             <br/>
+        //             <input type="submit" value="Login" name="Login"/>
+        //             <input type="submit" value="Create New Account" name="Create"/>
+        //         </form> */}
+        //         <button onClick={}>Login</button>
+        //         <button>Create New Account</button>
+        //     </div>
+        // )
