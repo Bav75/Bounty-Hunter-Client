65df3a71f0edcac813b99dad3190c63bd37fcd3a
Brian Velez <bvez94@gmail.com>
2019-10-07 11:42:58 -0700

    initial setup of LoginControl component. refactored App.js.

---
+++ src/components/login/LoginControl.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class LoginControl extends Component {
+
+    render() {
+        return (
+            <div>
+                Testing from LoginControl!
+            </div>
+        )
+    }
+}
