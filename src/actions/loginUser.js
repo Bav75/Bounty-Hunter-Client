a7574daa32ebad4d1d94b86fb1316e1c33f20f65
Brian Velez <bvez94@gmail.com>
2019-10-08 12:09:03 -0700

    refactored loginUser action to handle async request to api

--- src/actions/loginUser.js
+++ src/actions/loginUser.js
@@ -1,6 +1,42 @@
-export const loginUser = user => {
-    return {
-      type: 'LOGIN_USER',
-      user
-    };
+// export const loginUser = user => {
+//     return {
+//       type: 'LOGIN_USER',
+//       user
+//     };
+//   };
+
+const BASE_URL = 'http://localhost:3002';
+const LOGIN_URL = `${BASE_URL}/login`;
+
+export const loginUser = (user) => {
+
+  const configObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        user
+    })
   };
+  return (dispatch) => {
+    dispatch({ type: 'CHECKING_USER'})
+    fetch(LOGIN_URL, configObject).then(response => {return response.json()})
+    .then(responseJSON => {
+      if (responseJSON.hasOwnProperty("status")) {
+        console.log(`Status: ${responseJSON.status}`);
+        console.log(`Errors detected: ${responseJSON.errors}`);
+      } else {
+        dispatch({
+          type: "LOGIN_USER",
+          user: {
+            username: responseJSON.data.attributes.username,
+            password: responseJSON.data.attributes.password,
+            id: responseJSON.data.attributes.id
+          }
+        });
+      };
+  });
+  };
+};
