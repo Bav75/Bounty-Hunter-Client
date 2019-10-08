28fd4b78c5617d745cb16d037e694c2477bc95e2
Brian Velez <bvez94@gmail.com>
2019-10-08 10:47:47 -0700

    refactored createUser to perform async post to backend

--- src/actions/createUser.js
+++ src/actions/createUser.js
@@ -1,6 +1,41 @@
-export const createUser = user => {
-    return {
-      type: 'CREATE_USER',
-      user
-    };
+// export const createUser = user => {
+//     return {
+//       type: 'CREATE_USER',
+//       user
+//     };
+//   };
+
+const BASE_URL = 'http://localhost:3002';
+const USERS_URL = `${BASE_URL}/users`;
+
+export const createUser = (user) => {
+
+  const configObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        user
+    })
   };
+  return (dispatch) => {
+    dispatch({ type: 'CHECKING_USER'})
+    fetch(USERS_URL, configObject).then(response => {return response.json()})
+    .then(responseJSON => {
+      if (responseJSON.status < 300) {
+        dispatch({
+          type: "CREATE_USER",
+          user: {
+            username: responseJSON.data.attributes.username,
+            password: responseJSON.data.attributes.password
+          }
+        })
+      } else {
+        console.log(`Status: ${responseJSON.status}`);
+        console.log(`Errors detected: ${responseJSON.errors}`);
+      };
+  });
+  };
+};
