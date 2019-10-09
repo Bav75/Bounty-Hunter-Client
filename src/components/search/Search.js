ff13a77521e9f95d6da655cf9e3a8ab5d4068b45
Brian Velez <bvez94@gmail.com>
2019-10-09 11:26:15 -0700

    revised Search component new tab and added Mark bounty button

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Search = (props) => {
     return (
-    <li onClick={() => {window.open(props.link, '_blank')}}>{props.title}</li>
+    <li> <a onClick={() => {window.open(props.link, '_blank')}}>{props.title}</a> <button onClick={() => {console.log("Bounty Marked!")}}>Mark Bounty</button></li>
     );
 };
 
 export default Search;
+
+// onClick={() => {window.open(props.link, '_blank')}}
