dcf959a48d017737fbbe373c39fbfaddf73f4391
Brian Velez <bvez94@gmail.com>
2019-10-09 13:49:45 -0700

    created BountyContainer, BountyList, and Bounty components

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import usersReducer from './usersReducer';
 import sessionReducer from './sessionReducer';
 import searchReducer from "./searchReducer";
+import bountyReducer from "./bountyReducer";
 
 const rootReducer = combineReducers({
     user: usersReducer,
     session: sessionReducer,
-    searches: searchReducer
+    searches: searchReducer,
+    bounties: bountyReducer
 });
 
 export default rootReducer
