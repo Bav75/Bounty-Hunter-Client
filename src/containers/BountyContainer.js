08e0847f1ac458f6fb46b75e407e2c1a2b10a5ec
Brian Velez <bvez94@gmail.com>
2019-10-09 16:20:48 -0700

    extended BountyContainer component to handle rendering of marked bounties

--- src/containers/BountyContainer.js
+++ src/containers/BountyContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchBounties } from '../actions/fetchBounties';
+import BountyList from '../components/bounties/BountyList';
 
 class BountyContainer extends Component {
 
@@ -8,23 +9,23 @@ class BountyContainer extends Component {
         this.props.fetchBounties(this.props.user.id);
     };
 
-    // renderSearchList = () => {
-    //     if (this.props.searches.loading) {
-    //         return (
-    //             <p>Loading...</p>
-    //         );
-    //     } else {
-    //         return (
-    //             <SearchList searches={this.props.searches.content} markBounty={this.props.markBounty} userId={this.props.user.id}/>
-    //         );
-    //     };
-    // };
+    renderBountyList = () => {
+        if (this.props.bounties.loading) {
+            return (
+                <p>Loading marked bounties...</p>
+            );
+        } else {
+            return (
+                <BountyList bounties={this.props.bounties.content} />
+            );
+        };
+    };
 
     render() {
         return (
             <div className="BountyContainer">
                 I'm the bounty container 
-                {/* {this.renderSearchList()} */}
+                {this.renderBountyList()}
             </div>
         );
     };
