878f7718d9a279677bf2055ec2dcc67b683e33c9
Brian Velez <bvez94@gmail.com>
2019-10-10 15:27:08 -0700

    added styling to UserScreen component

--- src/components/user/UserScreen.js
+++ src/components/user/UserScreen.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import SearchContainer from '../../containers/SearchContainer';
 import BountyContainer from '../../containers/BountyContainer';
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 class UserScreen extends Component {
 
@@ -19,10 +20,12 @@ class UserScreen extends Component {
 
     render() {
         if (this.props.session) { 
-            return (
+            return ( 
                 <div className="UserScreen">
-                    <h1>Welcome to Bounty Hunter, {this.props.user.username}</h1>
-                    <h2>User id: {this.state.id}</h2>
+                    <Jumbotron>
+                        <h1>Welcome to Bounty Hunter, {this.props.user.username}</h1>
+                        <h2>User id: {this.state.id}</h2>
+                    </Jumbotron>
                     <SearchContainer />
                     <BountyContainer />
                 </div>
