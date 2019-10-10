67112731d40af33c98179bf96c6706dc345b9115
Brian Velez <bvez94@gmail.com>
2019-10-10 13:45:41 -0700

    implemented conversion of unix timestring on frontend

--- src/components/bounties/Bounty.js
+++ src/components/bounties/Bounty.js
@@ -4,6 +4,11 @@ import Accordion from 'react-bootstrap/Accordion'
 import Card from 'react-bootstrap/Card'
 
 const Bounty = (props) => {
+    console.log(props)
+
+    let unix_time = parseInt(props.bounty_closes_date * 1000);
+    let ending_date = new Date(unix_time);
+
     return (
         <Accordion>
             <Card>
@@ -12,8 +17,13 @@ const Bounty = (props) => {
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="0">
                     <Card.Body>
-                    {props.bounty_amount}
-                    {props.bounty_closes_date}
+                    Bounty Reward: +{props.bounty_amount} Reputation
+                    <br/>
+                    Bounty End Date: {ending_date.toString()}
+                    <br/>
+                    <a href={props.link}>View This Bounty</a>
+                    <br/>
+                    <button>Stop Tracking This Bounty</button>
                     </Card.Body>
                 </Accordion.Collapse>
                 
