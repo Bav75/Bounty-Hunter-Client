689d3765df863a0d87f81dc66712e5df293b9a58
Brian Velez <bvez94@gmail.com>
2019-10-10 13:59:02 -0700

    added removebounty action creator

---
+++ src/actions/removeBounty.js
@@ -0,0 +1,26 @@
+const BASE_URL = 'http://localhost:3002';
+const BOUNTY_URL = `${BASE_URL}/bounties`;
+
+export const removeBounty = (bountyId) => {
+
+    const configObject = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            bountyId
+        })
+      };
+
+    return (dispatch) => {
+        dispatch({type: 'LOADING_BOUNTIES'});
+        fetch(BOUNTY_URL, configObject).then(response => {return response.json()})
+        .then(responseJSON => {
+            dispatch({
+                type: 'RE_RENDER'
+            });
+        });
+    };
+};
