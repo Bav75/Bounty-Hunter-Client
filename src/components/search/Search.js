110f3a5635512b47899558180b51bb4f6da4f2f3
Brian Velez <bvez94@gmail.com>
2019-10-10 11:38:55 -0700

    trying to implement onHover color changes

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 import ListGroup from 'react-bootstrap/ListGroup'
 
 const Search = (props) => {
+
+
     return (
-    <ListGroup.Item>
+    <ListGroup.Item id="results">
         <a onClick={() => {window.open(props.link, '_blank')}}>{props.title}</a> 
         <button onClick={() => {props.markBounty(props)}}>Mark Bounty</button>
     </ListGroup.Item>
